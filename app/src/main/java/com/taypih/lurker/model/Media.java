72b9a512b9b289318874d6310dc6607d98520b2e
Taynara <taypih@gmail.com>
2019-11-23 23:10:51 -0400

    Create Media class

---
+++ app/src/main/java/com/taypih/lurker/model/Media.java
@@ -0,0 +1,10 @@
+package com.taypih.lurker.model;
+
+abstract public class Media {
+
+    abstract public String getUrl();
+
+    abstract public boolean isGif();
+
+    abstract public boolean isVideo();
+}
