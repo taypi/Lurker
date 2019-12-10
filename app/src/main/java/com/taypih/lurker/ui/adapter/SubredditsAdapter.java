7d831a0159e73ad498b87f2701006f617880b8ee
Taynara <taypih@gmail.com>
2019-12-10 01:44:39 -0400

    Create subreddits adapter

---
+++ app/src/main/java/com/taypih/lurker/ui/adapter/SubredditsAdapter.java
@@ -0,0 +1,43 @@
+package com.taypih.lurker.ui.adapter;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+
+import com.taypih.lurker.databinding.ItemSubredditBinding;
+import com.taypih.lurker.model.Subreddit;
+
+public class SubredditsAdapter extends ListAdapter<Subreddit, ViewHolder> {
+    public SubredditsAdapter() {
+        super(DIFF_CALLBACK);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        ItemSubredditBinding binding = ItemSubredditBinding.inflate(layoutInflater, parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(getItem(position));
+    }
+
+    static final DiffUtil.ItemCallback<Subreddit> DIFF_CALLBACK = new DiffUtil.ItemCallback<Subreddit>() {
+
+        @Override
+        public boolean areItemsTheSame(@NonNull Subreddit oldItem, @NonNull Subreddit newItem) {
+            return oldItem.getId().equals(newItem.getId());
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Subreddit oldItem, @NonNull Subreddit newItem) {
+            return oldItem.getId().equals(newItem.getId());
+        }
+    };
+}
