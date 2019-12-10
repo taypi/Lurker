e4cd86bba6a8e856f63a340a514d9fb7304368b0
Taynara <taypih@gmail.com>
2019-12-10 01:42:09 -0400

    Update commentsAdapter to use generic ViewHolder

--- app/src/main/java/com/taypih/lurker/ui/adapter/CommentsAdapter.java
+++ app/src/main/java/com/taypih/lurker/ui/adapter/CommentsAdapter.java
@@ -6,41 +6,26 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.taypih.lurker.databinding.ItemCommentBinding;
 import com.taypih.lurker.model.Comment;
 
-public class CommentsAdapter extends ListAdapter<Comment, CommentsAdapter.ViewHolder> {
+public class CommentsAdapter extends ListAdapter<Comment, ViewHolder> {
     public CommentsAdapter() {
         super(DIFF_CALLBACK);
     }
 
     @NonNull
     @Override
-    public CommentsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         ItemCommentBinding binding = ItemCommentBinding.inflate(layoutInflater, parent, false);
         return new ViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CommentsAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.bind(getItem(position));
-    }
-
-    class ViewHolder extends RecyclerView.ViewHolder {
-        private final ItemCommentBinding binding;
-
-        ViewHolder(ItemCommentBinding binding) {
-            super(binding.getRoot());
-            this.binding = binding;
-        }
-
-        void bind(Comment comment) {
-            binding.setModel(comment);
-            binding.executePendingBindings();
-        }
     }
 
     static final DiffUtil.ItemCallback<Comment> DIFF_CALLBACK = new DiffUtil.ItemCallback<Comment>() {
